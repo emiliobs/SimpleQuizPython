580cf2315be0a603f51d29de87ed4cd9530fb0b1
emilio barrera <barrera_emilio@yahoo.es>
2023-01-26 13:19:44 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+		# Simple Quiz In Python
+
+name = input("Please, Enter your Full  Name: - ")
+print(f'Hello "{name.upper()}" , Welcome to Python Quiz......')
+print("---------------------------------------------------------")
+
+while True:
+	total_question = 5
+	correct = 0
+
+
+print("---------------------------------------------------------")
+print("1. Start Quiz.")
+print("2. Exit.")
+print("---------------------------------------------------------")
+
+
